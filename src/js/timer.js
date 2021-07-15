a429b6f501a783317aec107aba44c67d675976c8
Balabukh Nick <nick_bron@ukr.net>
2021-07-15 14:09:05 +0300

    create function resetTimer

--- src/js/timer.js
+++ src/js/timer.js
@@ -2,10 +2,10 @@ import Swal from 'sweetalert2';
 import '../sass/main.scss';
 
 let varDate;
-  let timeInterval;
+let timeInterval;
 
 const refs = {
-  dateInput:  document.getElementById("date-selector"),
+  dateInput: document.getElementById('date-selector'),
   startBtn: document.querySelector('button[data-start]'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
@@ -13,55 +13,60 @@ const refs = {
   secs: document.querySelector('[data-seconds]'),
 };
 
-refs.startBtn.classList.add("disableButton");
-refs.dateInput.addEventListener("input", updateValue);
-refs.startBtn.addEventListener("click", startTimer);
+refs.startBtn.classList.add('disableButton');
+refs.dateInput.addEventListener('input', updateValue);
+refs.startBtn.addEventListener('click', startTimer);
 
+resetTimer();
+
+function resetTimer() {
+  refs.days.textContent = '00';
+  refs.hours.textContent = '00';
+  refs.mins.textContent = '00';
+  refs.secs.textContent = '00';
+  clearInterval(timeInterval);
+}
 
 function updateValue(e) {
-    
-    refs.days.textContent = "00";
-    refs.hours.textContent = "00";
-    refs.mins.textContent = "00";
-    refs.secs.textContent = "00";
-    clearInterval(timeInterval);
+  resetTimer();
 
-    varDate = new Date(e.target.value);
-    
-    if (varDate <= new Date()) {
-        refs.startBtn.classList.add("disableButton");
-       
-        Swal.fire({
-            title: 'Error!',
-            text: 'Please choose a date in the future',
-            icon: 'error',
-            confirmButtonText: 'OK'
-        })
-    }
-    else { refs.startBtn.classList.remove("disableButton");}
+  varDate = new Date(e.target.value);
+
+  if (varDate <= new Date()) {
+    refs.startBtn.classList.add('disableButton');
+
+    Swal.fire({
+      title: 'Error!',
+      text: 'Please choose a date in the future',
+      icon: 'error',
+      confirmButtonText: 'OK',
+    });
+  } else {
+    refs.startBtn.classList.remove('disableButton');
+  }
 }
 
 function startTimer(e) {
-    clearInterval(timeInterval);
-    timeInterval= setInterval(() => {
-        let ms = varDate.getTime() - new Date().getTime();
-        
-        if (ms < 0) {         
-            clearInterval(timeInterval);
-            return; }
-         
-          refs.days.textContent = pad(convertMs(ms).days);
-          refs.hours.textContent = pad(convertMs(ms).hours);
-          refs.mins.textContent = pad(convertMs(ms).minutes);
-          refs.secs.textContent = pad(convertMs(ms).seconds);
-    }, 1000);
-    
+  resetTimer();
+  timeInterval = setInterval(() => {
+    let ms = varDate.getTime() - new Date().getTime();
+
+    if (ms < 0) {
+      resetTimer();
+      return;
+    }
+
+    refs.days.textContent = pad(convertMs(ms).days);
+    refs.hours.textContent = pad(convertMs(ms).hours);
+    refs.mins.textContent = pad(convertMs(ms).minutes);
+    refs.secs.textContent = pad(convertMs(ms).seconds);
+  }, 1000);
 }
 
 function pad(value) {
-    return String(value).padStart(2, "0");
+  return String(value).padStart(2, '0');
 }
-  
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -80,5 +85,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-
